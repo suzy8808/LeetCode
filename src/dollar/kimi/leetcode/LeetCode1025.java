61c06319ca486c456ba48773d5807614032a5f17
XINGYIBO <1422798050@qq.com>
2020-04-15 19:48:56 +0900

    2020/4/15

--- src/dollar/kimi/leetcode/LeetCode1025.java
+++ src/dollar/kimi/leetcode/LeetCode1025.java
@@ -2,6 +2,18 @@ package dollar.kimi.leetcode;
 
 public class LeetCode1025 {
 	public boolean divisorGame(int N) {
-
+		if (N == 1)
+			return false;
+		boolean[] dp = new boolean[N + 1];
+		dp[2] = true;
+		for (int i = 3; i < N + 1; i++) {
+			for (int j = 1; j < i; j++) {
+				if (i % j == 0) {
+					if (dp[i - j] == false)
+						dp[i] = true;
+				}
+			}
+		}
+		return dp[N];
 	}
 }
