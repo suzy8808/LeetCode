d5b1bd27f6a1f4150203a75f5689167570ee78da
DollarKiMi <1422798050@qq.com>
2020-05-27 17:39:42 +0800

    2020/5/27

--- src/dollar/kimi/MainTest.java
+++ src/dollar/kimi/MainTest.java
@@ -1,15 +1,26 @@
 package dollar.kimi;
 
+<<<<<<< HEAD
 import dollar.kimi.leetcode.LeetCode210;
 
 import java.util.concurrent.atomic.AtomicInteger;
+=======
+import dollar.kimi.leetcode.LeetCode105;
+import dollar.kimi.leetcode.LeetCode5;
+import dollar.kimi.leetcode.LeetCode76;
+>>>>>>> 0715d4167f6cec995ec60c9f49a4b6c1d16813d1
 
 public class MainTest {
 	// ['MinStack','push','push','push','top','pop','getMin','pop','getMin','pop','push','top','getMin','push','top','getMin','pop','getMin']
 	// [[],[2147483646],[2147483646],[2147483647],[],[],[],[],[],[],[2147483647],[],[],[-2147483648],[],[],[],[]]
 	public static void main(String[] args) throws InterruptedException {
+<<<<<<< HEAD
 		LeetCode210 leet=new LeetCode210();
 		leet.findOrder(2,new int[][]{{0,1},{1,0}});
+=======
+		LeetCode76 leet=new LeetCode76();
+		System.out.println(leet.minWindow("a","a"));
+>>>>>>> 0715d4167f6cec995ec60c9f49a4b6c1d16813d1
 	}
 
 }
